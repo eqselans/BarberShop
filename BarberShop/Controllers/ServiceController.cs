84ebd66810f12a76d2cfff3f544b6509c969f93f
eqselans <emrhn.axu.soft1@gmail.com>
2024-12-08 15:58:07 +0300

    Proje dosyası ekle.

---
+++ BarberShop/Controllers/ServiceController.cs
@@ -0,0 +1,41 @@
+﻿using BarberShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BarberShop.Data;
+
+
+namespace BarberShop.Controllers
+{
+    public class ServicesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ServicesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var services = await _context.Services.ToListAsync();
+            return View(services);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Services.Add(service);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(service);
+        }
+    }
+}
---
+++ BarberShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BarberShop.Models; // Modellerinizi içeren namespace'i ekleyin.
+
+namespace BarberShop.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSet tanımları (Hangi modellerin veritabanına bağlanacağını belirtin)
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+    }
+}
+
